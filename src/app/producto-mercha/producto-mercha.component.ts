454c69eb274de88b298ee152d3fb92de1d116a1c
birbi <birbi.bass@gmail.com>
2021-04-23 16:48:29 +0200

    components Header_Footer + atributos y Metodos

--- src/app/producto-mercha/producto-mercha.component.ts
+++ src/app/producto-mercha/producto-mercha.component.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./producto-mercha.component.css']
 })
 export class ProductoMerchaComponent implements OnInit {
+  //atributos, propiedades
+  private codigo_producto = "cp0001";
+  private nombre_producto = "Camiseta Freddy";
+  private precio_producto = 17;
+  private descripcion_producto = "Camiseta de algodon, Tallas: S, M , L, XL, XXL";
+  private imagen_producto = "mostrar imagen";
 
+  //metodos getter & setter
+  get_nombre(){
+    return this.nombre_producto;
+  }
+
+  get_precio(){
+    return this.precio_producto;
+  }
+
+  get_descripcion(){
+    return this.descripcion_producto;
+  }
+
+  get_imagen(){
+    return this.imagen_producto;
+  }
+
+  //constructor
   constructor() { }
 
   ngOnInit(): void {

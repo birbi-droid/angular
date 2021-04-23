eed3d91929a06f6f2b44f414ae683e2c54f0b739
birbi <birbi.bass@gmail.com>
2021-04-23 12:26:17 +0200

    components Header_Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductosMerchaComponent } from './productos-mercha/productos-mercha.component';
 import { ProductoMerchaComponent } from './producto-mercha/producto-mercha.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductosMerchaComponent,
-    ProductoMerchaComponent
+    ProductoMerchaComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

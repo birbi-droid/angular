28ecf2a2deee082b9ac0b7596a72ef4a06e9adf3
birbi <birbi.bass@gmail.com>
2021-04-24 01:00:07 +0200

    Header Foooter bootstrap fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { ProductosMerchaComponent } from './productos-mercha/productos-mercha.co
 import { ProductoMerchaComponent } from './producto-mercha/producto-mercha.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormRegisterComponent } from './form-register/form-register.component';
+import { FormLoginComponent } from './form-login/form-login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { FooterComponent } from './footer/footer.component';
     ProductosMerchaComponent,
     ProductoMerchaComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FormRegisterComponent,
+    FormLoginComponent
   ],
   imports: [
     BrowserModule,
